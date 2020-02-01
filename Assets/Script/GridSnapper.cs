e03946e5532d2a6a0b4877769ab6eb745ff9a573
SardineFish <Sardine@live.cn>
2020-02-01 15:24:49 +0800

    Import Assets.

---
+++ Assets/Script/GridSnapper.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+public enum SnapMode
+{
+    None,
+    SnapToPixel,
+    SnapToGrid
+}
+public class GridSnapper : MonoBehaviour
+{
+    public float PixelPerUnit = 8;
+    public SnapMode SnapMode = SnapMode.SnapToPixel;
+    public Vector3 OriginOffset;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
